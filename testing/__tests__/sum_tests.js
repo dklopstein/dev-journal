8c31236240b941d93e6a3cec7206ff3aa10f023e
NathanD0wd <122576040+NathanD0wd@users.noreply.github.com>
2024-05-10 14:27:48 -0700

    Create sum_tests.js

---
+++ testing/__tests__/sum_tests.js
@@ -0,0 +1,11 @@
+// sum.test.js
+
+import { sum } from '../code_to_test/sum';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1,2)).toBe(3);
+});
