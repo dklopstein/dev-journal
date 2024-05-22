61d62e962c3510d01c17d6a743c8de445deccb3e
kainoaw <kwoessner@ucsd.edu>
2024-05-22 14:48:21 -0700

    FIxed codecy

--- source/calendar/calendar.js
+++ source/calendar/calendar.js
@@ -1,12 +1,13 @@
 // Wait for window to load
 window.addEventListener('DOMContentLoaded', init);
 
+// initialize date variables
+let date = new Date();
+let currMonth = date.getMonth();
+let currYear = date.getFullYear();
+
 // When page loads
 function init(){
-    // initialize date variables
-    let date = new Date();
-    let currMonth = date.getMonth();
-    let currYear = date.getFullYear();
 
     // Get Jump variables and list
     let createYearRange = gen_years(currYear-10, currYear+10); 
@@ -18,7 +19,7 @@ function init(){
     // Header of Days of the week
     let HDaysOfWeek = "<tr>";
     // Loop through allDays list
-    for (day of allDays){
+    for (let day of allDays){
         HDaysOfWeek += "<th data-days='" + day + "'>" + day + "</th>";
     }
     // Close header
@@ -63,7 +64,7 @@ function init(){
 // Function to goto next month
 function next(){
     // If Dec to Jan increment year
-    if (currMonth == 11){
+    if (currMonth === 11){
         currYear += 1;
     }
     // Increment currMonth and go to 0 if Dec to Jan
@@ -77,7 +78,7 @@ function prev(){
     // Decrement currMonth
     currMonth--;
     // If Jan to Dec decrement year and set month to Dec
-    if (currMonth == -1){
+    if (currMonth === -1){
         currYear -= 1;
         currMonth = 11;
     }
@@ -101,9 +102,7 @@ function calendarButton() {
  
 // Function to display the calendar
 function displayCalendar(mnth, yr){
-    // Get today's date
-    let date = new Date();
-    
+
     // Initialize list of months
     let allMonths = [
         "January","February","March","April","May","June",
@@ -138,7 +137,7 @@ function displayCalendar(mnth, yr){
     // Set previous month and year variables
     let prevMonth = mnth - 1;
     let prevYear = yr;
-    if (mnth == 0){
+    if (mnth === 0){
         prevMonth = 11;
         prevYear = yr-1;
     }
@@ -147,7 +146,7 @@ function displayCalendar(mnth, yr){
     // Set previous month and year variables
     let nextMonth = mnth + 1;
     let nextYear = yr;
-    if (mnth == 11){
+    if (mnth === 11){
         nextMonth = 0;
         nextYear = yr+1;
     }
@@ -165,14 +164,14 @@ function displayCalendar(mnth, yr){
         // Loop through number of columns
         for (let j = 0; j < 7; j++) {
             // Create data for each table cell in the row
-            cell_data = document.createElement("td");
+            let cell_data = document.createElement("td");
 
             // Fill in previous month into unfilled cells before first day of month
-            if (i == 0 && j < firstDay) {
+            if (i === 0 && j < firstDay) {
                 // Calculate dates of previous month
                 let prevMonthDay = daysInMonth(prevMonth, prevYear) - (firstDay - j) + 1;
                 // Number of current day
-                cellNum = document.createElement('span');
+                let cellNum = document.createElement('span');
                 cellNum.textContent = prevMonthDay;
                 cellNum.className = "cell-date";
 
@@ -190,7 +189,7 @@ function displayCalendar(mnth, yr){
             // Fill next month into unfilled cells after last day of month
             else if (currMonthDay > daysInMonth(mnth, yr)) {
                 // Number of current day
-                cellNum = document.createElement('span');
+                let cellNum = document.createElement('span');
                 cellNum.textContent = nextMonthDay;
                 // Add class date-num and other-month
                 cell_data.className = "other-month-date-num";
@@ -211,7 +210,7 @@ function displayCalendar(mnth, yr){
             else {
                 // Number of current day
             //    cellNum = document.createTextNode(currMonthDay);
-                cellNum = document.createElement('span');
+                let cellNum = document.createElement('span');
                 cellNum.textContent = currMonthDay;
                 cellNum.className = "cell-date";
                 // Add class date-num
