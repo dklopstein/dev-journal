3efaa915f5c49fa89f2fb80f23672c91be3e689d
kainoaw <kwoessner@ucsd.edu>
2024-05-29 22:02:56 -0700

    Added live server to puppeteer.yml to run e2e tests in the pipeline

--- testing/__tests__/puppeteer.test.js
+++ testing/__tests__/puppeteer.test.js
@@ -1,14 +1,14 @@
 describe('Basic user path in homepage', () => {
+  // Visit dev journal using live server
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500/source/homepage/homepage.html');
+  });
 
   // Edit Journal
   it('Click into journal, type, click out', async () => {
     console.log('...');
-    
-  // First visit dev journal website with github pages
-  await page.goto('https://cse110-sp24-group25.github.io/cse110-sp24-group25/source/homepage/homepage.html');
 
   // Visit dev journal using live server
-  await page.goto('http://127.0.0.1:5500/source/homepage/homepage.html');
 
   console.log('Typing into journal');
         
