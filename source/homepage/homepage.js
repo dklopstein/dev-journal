08f104a4811caeaca732788d2c0c702206fc93db
Christopher Edwards <66543275+chedwards492@users.noreply.github.com>
2024-06-02 22:05:35 -0700

    Clean up unused code

--- source/homepage/homepage.js
+++ source/homepage/homepage.js
@@ -115,8 +115,6 @@ function addTask() {
     const taskList = document.querySelector(".task-container");
     const task = document.createElement("li");
     task.setAttribute("class", "task");
-
-    /* WORKS ON CHROME, NOT FIREFOX */
     task.insertAdjacentHTML("beforeend", `
         <div class="check-input-wrap">
             <button id="task1" class="task-checkbox"></button>
@@ -334,24 +332,9 @@ function dateQuery() {
     }
 }
 
-
-function clickTaskList() {
-    const taskList = document.querySelector('.task-list');
-    const outSide = document.querySelector('.main-wrap');
-    taskList.addEventListener('click', function(event) {
-        if (event.target === taskList) {
-            if (window.innerWidth <= 800) { 
-                taskList.classList.toggle('active');
-            }
-        }
-    });
-    outSide.addEventListener('click', function(event){
-        if (window.innerWidth <= 800) { 
-            taskList.classList.remove('active');
-        }
-    });
-}
-
+/**
+ * Set task bullet point color on past week view
+ */
 function taskColor(){
     // Get all elements with class .task-item
     const taskItems = document.querySelectorAll('.task-item');
