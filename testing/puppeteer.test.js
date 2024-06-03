e9179568913d19155c76e388491d72353ed7789a
NathanD0wd <dowdnj@gmail.com>
2024-06-03 15:42:15 -0700

    debugging

--- testing/puppeteer.test.js
+++ testing/puppeteer.test.js
@@ -6,122 +6,122 @@ describe('Basic user path in homepage', () => {
   });
 
 
-  // Edit Journal
-  it('Click into journal, type, click out', async () => {
-    console.log('Editing journal...');
+  // // Edit Journal
+  // it('Click into journal, type, click out', async () => {
+  //   console.log('Editing journal...');
 
-    // Wait for page to load
-    await page.waitForSelector('#textarea');
+  //   // Wait for page to load
+  //   await page.waitForSelector('#textarea');
 
-    const journal = await page.$('#textarea');
-    // Click into text area
-    await journal.click();
+  //   const journal = await page.$('#textarea');
+  //   // Click into text area
+  //   await journal.click();
 
-    // Define string to type into journal
-    const input_text = 'Example journal entry: I was so productive today!!';
-    // Type into text area
-    await page.keyboard.type(input_text);
-    // Click out of the text area
-    await page.click('#current-date');
+  //   // Define string to type into journal
+  //   const input_text = 'Example journal entry: I was so productive today!!';
+  //   // Type into text area
+  //   await page.keyboard.type(input_text);
+  //   // Click out of the text area
+  //   await page.click('#current-date');
 
-    // Get journal text
-    const text = await journal.getProperty('value');
-    const journal_text = await text.jsonValue();
+  //   // Get journal text
+  //   const text = await journal.getProperty('value');
+  //   const journal_text = await text.jsonValue();
 
-    // Expect journal text to be jour
-    expect(journal_text).toBe(input_text);
-  });
+  //   // Expect journal text to be jour
+  //   expect(journal_text).toBe(input_text);
+  // });
 
 
-  // Select happy in feelings widget
-  it('Click happiest rating in feelings widget', async () => {
-    console.log('Testing feelings widget first click...');
+  // // Select happy in feelings widget
+  // it('Click happiest rating in feelings widget', async () => {
+  //   console.log('Testing feelings widget first click...');
 
-    // Click happiest rating button
-    const happy = await page.$('#btn4');
-    await happy.click();
+  //   // Click happiest rating button
+  //   const happy = await page.$('#btn4');
+  //   await happy.click();
 
-    // Check class name to include active
-    const class_name = await page.evaluate(() => {
-      const img = document.querySelector('#btn4 img');
-      return img.className;
-    });
+  //   // Check class name to include active
+  //   const class_name = await page.evaluate(() => {
+  //     const img = document.querySelector('#btn4 img');
+  //     return img.className;
+  //   });
 
-    // Expect active got added to happy class name
-    expect(class_name).toBe("active");
-  });
+  //   // Expect active got added to happy class name
+  //   expect(class_name).toBe("active");
+  // });
 
-  // Select mad in feelings widget
-  it('Click mad rating in feelings widget', async () => {
-    console.log('Testing feelings widget second click...');
+  // // Select mad in feelings widget
+  // it('Click mad rating in feelings widget', async () => {
+  //   console.log('Testing feelings widget second click...');
 
-    // Click happiest rating button
-    const mad = await page.$('#btn1');
-    await mad.click();
+  //   // Click happiest rating button
+  //   const mad = await page.$('#btn1');
+  //   await mad.click();
 
-    // Get img element for happy and mad
-    const img_happy = await page.$('#btn2 img');
-    const img_mad = await page.$('#btn1 img');
+  //   // Get img element for happy and mad
+  //   const img_happy = await page.$('#btn2 img');
+  //   const img_mad = await page.$('#btn1 img');
 
-    // Store class name of both happy and mad
-    const class_name_happy = await page.evaluate(img_happy => {
-      return img_happy.getAttribute('class');
-    }, img_happy);
-    const class_name_mad = await page.evaluate(img_mad => {
-      return img_mad.getAttribute('class');
-    }, img_mad);
+  //   // Store class name of both happy and mad
+  //   const class_name_happy = await page.evaluate(img_happy => {
+  //     return img_happy.getAttribute('class');
+  //   }, img_happy);
+  //   const class_name_mad = await page.evaluate(img_mad => {
+  //     return img_mad.getAttribute('class');
+  //   }, img_mad);
 
-    console.log(class_name_mad);
+  //   console.log(class_name_mad);
 
-    // Expect active removed from happy and added to mad
-    expect(class_name_mad).toBe("active");
-    expect(class_name_happy).toBe('');
-  });
+  //   // Expect active removed from happy and added to mad
+  //   expect(class_name_mad).toBe("active");
+  //   expect(class_name_happy).toBe('');
+  // });
 
-  // Select 5 in productivity widget
-  it('Click 5 rating in productivity widget', async () => {
-    console.log('Testing productivity widget first click...');
+  // // Select 5 in productivity widget
+  // it('Click 5 rating in productivity widget', async () => {
+  //   console.log('Testing productivity widget first click...');
 
-    // Click 5 productivity rating button
-    const pro5 = await page.$('#btn9');
-    await pro5.click();
+  //   // Click 5 productivity rating button
+  //   const pro5 = await page.$('#btn9');
+  //   await pro5.click();
 
-    // Get img element
-    const img = await page.$('#btn9 img');
-    // Get class name of 1 productivity
+  //   // Get img element
+  //   const img = await page.$('#btn9 img');
+  //   // Get class name of 1 productivity
 
-    const class_name = await page.evaluate(img => {
-      return img.getAttribute('class');
-    }, img);
+  //   const class_name = await page.evaluate(img => {
+  //     return img.getAttribute('class');
+  //   }, img);
 
-    // Expect active to be added to class name of 5 img
-    expect(class_name).toBe("active");
-  });
+  //   // Expect active to be added to class name of 5 img
+  //   expect(class_name).toBe("active");
+  // });
 
-  // Select 1 in productivity widget
-  it('Click 1 rating in productivity widget', async () => {
-    console.log('Testing productivity widget second click...');
+  // // Select 1 in productivity widget
+  // it('Click 1 rating in productivity widget', async () => {
+  //   console.log('Testing productivity widget second click...');
 
-    // Click 1 productivity rating button
-    const pro1 = await page.$('#btn6');
-    await pro1.click();
+  //   // Click 1 productivity rating button
+  //   const pro1 = await page.$('#btn6');
+  //   await pro1.click();
 
-    // Get img elements
-    const img_1 = await page.$('#btn6 img');
-    const img_5 = await page.$('#btn9 img');
+  //   // Get img elements
+  //   const img_1 = await page.$('#btn6 img');
+  //   const img_5 = await page.$('#btn9 img');
 
-    // Get class name of 1 and 5 productivity imgs
-    const class_name_1 = await page.evaluate(img_1 => {
-      return img_1.getAttribute('class');
-    }, img_1);
-    const class_name_5 = await page.evaluate(img_5 => {
-      return img_5.getAttribute('class');
-    }, img_5);
+  //   // Get class name of 1 and 5 productivity imgs
+  //   const class_name_1 = await page.evaluate(img_1 => {
+  //     return img_1.getAttribute('class');
+  //   }, img_1);
+  //   const class_name_5 = await page.evaluate(img_5 => {
+  //     return img_5.getAttribute('class');
+  //   }, img_5);
 
-    // Expect active to be on img1 and not img5
-    expect(class_name_1).toBe("active");
-    expect(class_name_5).toBe("");
-  });
+  //   // Expect active to be on img1 and not img5
+  //   expect(class_name_1).toBe("active");
+  //   expect(class_name_5).toBe("");
+  // });
 
   it('Add a task, set its title, and choose a color', async () => {
     console.log('Testing task addition, title setting, and color selection...');
@@ -233,7 +233,7 @@ describe('Basic user path in homepage', () => {
     const taskListClass = await page.evaluate(() => {
       return document.querySelector('.task-list').className;
     });
-    
+
     // Expect the task-list to not have 'active' class after clicking the main-wrap
     expect(taskListClass.includes('active')).toBe(false);
   });
