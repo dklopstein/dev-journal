a3d8a091fba3b9fb7b4f894486344d607425b67d
NathanD0wd <dowdnj@gmail.com>
2024-06-05 12:18:27 -0700

    false to true in puppeteer config

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   launch: {
-    headless: false, /* Change to false if you want to see the tests run on a browser */
+    headless: true, /* Change to false if you want to see the tests run on a browser */
     slowMo: 25,
     defaultViewport: null
   }
