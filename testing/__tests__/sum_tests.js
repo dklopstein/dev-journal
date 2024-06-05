37f6d925698cda1d9bfbbe04caefce3f7227374f
RadioMediocre <qiwen108@gmail.com>
2024-06-04 17:44:03 -0700

    Update sum_tests.js

--- testing/__tests__/sum_tests.js
+++ testing/__tests__/sum_tests.js
@@ -3,28 +3,44 @@
 import { 
   saveToStorage,
   loadFromStorage,
-  getJournal,
-  // saveTasks,
-  // getTasks,
-  // loadTasks,
-  // saveCompleted,
-  // getCompleted,
-  // loadCompleted,
-  // saveWidgets,
-  // loadWidgets
+  getJournal
 
 } from '../../source/homepage/homepage.js';
 
 beforeEach(() => {
 });
 
-test('save and load', () => {
+test('Single saveToStorage and loadFromStorage', () => {
+  let journals = [];
+  let dateText = "5/30/2024";
+  saveToStorage(journals, dateText, "rating", 5)
+  expect(loadFromStorage(journals, dateText, "rating")).toBe(5);
+});
+
+test('Multiple saveToStorage and loadFromStorage', () => {
   let journals = [];
   let dateText = "5/30/2024";
   saveToStorage(journals, dateText, "rating", 5)
   saveToStorage(journals, dateText, "productivity", 8)
+  let dateText2 = "5/31/2024";
+  saveToStorage(journals, dateText2, "rating", 3)
+  saveToStorage(journals, dateText2, "productivity", 7)
   expect(loadFromStorage(journals, dateText, "rating")).toBe(5);
   expect(loadFromStorage(journals, dateText, "productivity")).toBe(8);
+  expect(loadFromStorage(journals, dateText2, "rating")).toBe(3);
+  expect(loadFromStorage(journals, dateText2, "productivity")).toBe(7);
+});
+
+test('loadFromStorage null check', () => {
+  let journals = [];
+  let dateText = "4/1/2024";
+  expect(loadFromStorage(journals, dateText, "rating")).toBe(null);
+});
+
+test('getJournal null check', () => {
+  expect(getJournal()).toStrictEqual({});
+
+  
 });
 
 test('getJournal', () => {
@@ -34,8 +50,6 @@ test('getJournal', () => {
       productivity: 8
     }
   };
-  let dateText = "5/30/2024";
   localStorage.setItem("journals", JSON.stringify(journals));
   expect(getJournal()).toStrictEqual(journals);
 });
-
