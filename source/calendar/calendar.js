1fc590b8970fdbfb7037aa55c5646a0519d0f2d4
Seongah Kim <kimsa0322@naver.com>
2024-06-07 13:33:33 -0700

    Standardized styling in calendar.css

--- source/calendar/calendar.js
+++ source/calendar/calendar.js
@@ -115,7 +115,7 @@ function addTask(loadTask = false) {
     // Add a task to an element of task container
     const taskList = document.querySelector(".task-container");
     const task = document.createElement("li");
-    task.setAttribute("class", "task");
+    task.className = "task";
     
     // Add it at the first row
     task.insertAdjacentHTML("beforeend", `
@@ -130,7 +130,7 @@ function addTask(loadTask = false) {
             <button id="pink" class="color-button"></button>
             <button id="grey" class="color-button"></button>
         </div>
-        <img class="fas fa-trash-alt" src="../icons/trash-icon.svg" alt="Remove">
+        <img class="trash-icon" src="../icons/trash-icon.svg" alt="Remove">
     `);
     task.querySelector(".task-input").addEventListener("input", saveTasks);
 
@@ -203,7 +203,7 @@ function taskButtonsFunctionality(task) {
     });
 
     // Trash icon delete functionality
-    const deleteIcon = task.querySelector(".fas");
+    const deleteIcon = task.querySelector(".trash-icon");
     deleteIcon.addEventListener("click", () => {
         task.remove();
         saveTasks();
